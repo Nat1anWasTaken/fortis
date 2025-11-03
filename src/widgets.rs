50b7dcaea1deb8c8ac40521ad0c5968db34c62c8
Nat1anWasTaken <nathan@nat1an.xyz>
2025-11-03 12:02:23 +0800

    feat: hello tauri

--- src/widgets.rs
+++
@@ -1,7 +0,0 @@
-mod device_dialog;
-mod footer;
-mod transcriptions;
-
-pub use device_dialog::{DeviceDialog, DeviceDialogState};
-pub use footer::FooterWidget;
-pub use transcriptions::{TranscriptionMessage, TranscriptionWidget, TranscriptionWidgetState};
